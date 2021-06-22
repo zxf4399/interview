8225a5ab23e46e6c3447d1a964f3db5af03c0f6d
郑贤锋 <zxf4399@gmail.com>
2021-06-22 14:00:38 +0800

    feat: add webpack demo2

---
+++ webpack/webpack5/demo2/webpack.config.js
@@ -0,0 +1,42 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = {
+  mode: 'production',
+  entry: {
+    a: './src/a.js',
+    b: './src/b.js',
+  },
+  output: {
+    clean: true,
+    // filename: '[name].[chunkhash].bundle.js',
+    filename: '[name].[contenthash].bundle.js',
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      // filename: '[chunkhash].css',
+      filename: '[contenthash].css',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+    ],
+  },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        styles: {
+          name: 'styles',
+          type: 'css/mini-extract',
+          // For webpack@4
+          // test: /\.css$/,
+          chunks: 'all',
+          enforce: true,
+        },
+      },
+    },
+  },
+}
